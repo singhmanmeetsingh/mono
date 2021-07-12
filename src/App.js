27f1fe5340baff056c42d5a24e82ca76affe2e12
singhmanmeetsingh <singhmanmeet523@gmail.com>
2021-07-12 20:54:14 +0530

    svg created and loader added

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import React, { Suspense, useEffect } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import './App.scss';
 import MainRoute from "./Route";
+import loaderImg from './assets/images/animated_loader.gif'
  const App =() => {
 
-  // const Button = React.lazy(() => {
-  //   return import("./routes/Buttons/Button");
-  // });
-  // const route =(
-  //   <Switch>
-  //       <Route path="/button" render={(props) => <Button {...props} />} />
-  //   </Switch>
-  // )
+  const Loader = () => (
+    <>
+      <img src={loaderImg} className="loader"/>
+    </>
+  )
 
   return (
     <>
-    <Suspense fallback={<div>Loading...</div>}><MainRoute/></Suspense>
+    <Suspense fallback={<Loader/>}><MainRoute/></Suspense>
     </>
   );
 }
